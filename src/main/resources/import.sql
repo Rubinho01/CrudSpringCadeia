6776c0b406479cf67f190127782c9fa48b4d18cb
Rubinho01 <rubemkruger123@gmail.com>
2024-11-15 20:56:42 -0300

    Criação dos repositoórios

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO works(work_name,time_reduction) VALUES ('Sweep_floor',7);
+INSERT INTO works(work_name,time_reduction) VALUES ('Wash_Dishes',3);
+INSERT INTO works(work_name,time_reduction) VALUES ('Breaking_Stone',60);
+asdasdasd;
